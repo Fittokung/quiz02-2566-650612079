f39dead73192d3d17bb2294fd254b9f9cfbe698d
Okinawa <fitthep@gmail.com>
2023-08-10 15:29:47 +0700

    Unfinish

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,8 @@
 
 import { Comment } from "@/components/Comment";
 import { PostOwner } from "@/components/PostOwner";
+import { Reply } from "@/components/Reply";
 import { comments } from "@/libs/comments";
-import { reply } from "@/components/Reply";
 
 export default function HomePage() {
   return (
@@ -39,27 +39,15 @@ export default function HomePage() {
 
         {/* Reply Example */}
         <div className="d-flex gap-2 my-2 ps-5">
-          <img
-            src="/profileImages/puppy.jpg"
-            width="48"
-            height="48"
-            className="rounded-circle"
-            style={{ objectFit: "cover" }}
-          />
-          <div
-            className="rounded rounded-3 p-2"
-            style={{ backgroundColor: "#3A3B3C" }}
-          >
-            <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
-              หมาน้อย
-            </span>
-            <br />
-            <span style={{ color: "#E4E6EB" }}>จริงค้าบบบบบบบบ</span>
-            <div className="d-flex align-items-center gap-1">
-              <img src="/like.svg" width={20}></img>
-              <span style={{ color: "#B0B3B8" }}>2 คน</span>
-            </div>
-          </div>
+          {comments.map((comments) => (
+            <Reply
+              userImagePath={comments.replies.userImagePath}
+              username={comments.replies.username}
+              replyText={comments.replies.commentText}
+              likeNum={comments.replies.likeNum}
+              replies={comments.replies.replies}
+            />
+          ))}
         </div>
 
         {/* map-loop render Comment component here */}
